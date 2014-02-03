65568416f071420fdcd4df09fe1d705372748ebc
Andrew Marrone <github@andrewmarrone.com>
2014-02-02 19:55:23 -0800

    started knight problem
    didn’t get far

---
+++ Week_1/Day_5/knight.rb
@@ -0,0 +1,11 @@
+require 'treenode.rb'   # for node traversing
+
+class KnightPathFinder
+   
+   def initialize(starting_position = [0,1])
+      @starting_position = starting_position
+   end
+   
+   def build_move_tree
+   end
+end
